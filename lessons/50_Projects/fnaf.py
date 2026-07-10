0f1baad444893328bbacbc350f575be1b090a37b
League Student <student@jointheleague.org>
2026-07-10 13:07:01 -0700

    Auto-commit: workspace changes before push

--- lessons/50_Projects/fnaf.py
+++ lessons/50_Projects/fnaf.py
@@ -13,11 +13,10 @@ screen.setup(500, 500)
 cam_colors = ["red", "black","white", "blue","green"]
 
 def show_animatronics(cam_num):
-    if  (cam_num == tina_path[tina_progress])
+    if  (cam_num == tina_path[tina_progress]):
         tina.showturtle()
-
- else:
-    tina.hideturtle
+    else:
+        tina.hideturtle()
 
 def open_cam_1():
   print("cam 1 open")
