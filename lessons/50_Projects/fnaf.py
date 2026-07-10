617688da9a42edf1455e873e5d6a6ea082e52e94
League Student <student@jointheleague.org>
2026-07-10 13:19:11 -0700

    Automated commit

--- lessons/50_Projects/fnaf.py
+++ lessons/50_Projects/fnaf.py
@@ -17,6 +17,7 @@ def move_tina():
     tina_progress += 1
     screen.ontimer(move_tina,2000)
 def show_animatronics(cam_num):
+    global cam num 
     if  (cam_num == tina_path[tina_progress]):
         tina.showturtle()
     else:
