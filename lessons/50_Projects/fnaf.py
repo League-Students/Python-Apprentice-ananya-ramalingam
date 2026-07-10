880103d08483dd8cf8d02b6deee86fd934d47b59
League Student <student@jointheleague.org>
2026-07-10 13:01:31 -0700

    Automated commit

--- lessons/50_Projects/fnaf.py
+++ lessons/50_Projects/fnaf.py
@@ -13,7 +13,8 @@ screen.setup(500, 500)
 cam_colors = ["red", "black","white", "blue","green"]
 
 def show_animatronics(cam_num):
-if(cam_num == tina_path)
+if(cam_num == tina_path[tina_progress])
+
 
 def open_cam_1():
   print("cam 1 open")
