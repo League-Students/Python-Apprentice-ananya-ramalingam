b7107df7e846753a1b4a4720664d989f3362e456
League Student <student@jointheleague.org>
2026-07-10 13:09:01 -0700

    Auto-commit: workspace changes before push

--- lessons/50_Projects/fnaf.py
+++ lessons/50_Projects/fnaf.py
@@ -30,13 +30,13 @@ def open_cam_3():
     print("cam 3 open")
     screen.bgcolor(cam_colors[2])
 
-    def open_cam_4():
+def open_cam_4():
     print("cam 4 open")
 
-screen.bgcolor(cam_colors[3])
+    screen.bgcolor(cam_colors[3])
 
     def open_cam_5():
-print("cam 5 open")
+        print("cam 5 open")
 
 def exit_cam()
 
